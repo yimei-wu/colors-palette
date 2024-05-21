9a4f1e5a663978e428f1db3d8cfebabf28cec584
yimei-wu <wuyimei60@gmail.com>
2024-05-21 10:43:20 +0200

    init

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import "./App.css";
+import Step1 from "./components/Step1";
+
+function App() {
+  return (
+    <div>
+      <h1>Colors Palette</h1>
+    </div>
+  );
+}
+
+export default App;
