b1e3e5ba442433a94cf9b23af75059a79b211072
yimei-wu <wuyimei60@gmail.com>
2024-05-27 23:55:43 +0200

    fetch data from the color api

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,61 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import ColorsPalette from "./components/ColorsPalette";
 import "./App.css";
 
 function App() {
-  const [colors, setColors] = useState([
-    "#22223B",
-    "#4A4E69",
-    "#9A8C98",
-    "#C9ADA7",
-    "#F2E9E4",
-  ]);
+  // const [colors, setColors] = useState([
+  //   "#22223B",
+  //   "#4A4E69",
+  //   "#9A8C98",
+  //   "#C9ADA7",
+  //   "#F2E9E4",
+  // ]);
 
-  const updateAllColors = () => {
-    const newColors = colors.map((color) => {
+  const [color, setColor] = useState([]);
+
+  useEffect(() => {
+    fetch("https://www.thecolorapi.com/id?hex=c9ada7")
+      .then((response) => response.json())
+      .then((data) => {
+        setColor([data.hex.value]);
+        console.log(data);
+      })
+      .catch((error) => console.error("Error:", error));
+  }, []);
+
+  // const [selectedColorId, setSelectedColorId] = useState([""]);
+
+  // <input
+  //   type="color"
+  //   value={selectedColorId}
+  //   onChange={(e) => setSelectedColorId(e.target.value)}
+  //   placeholder="Inserisci l'ID del colore"
+  // />;
+
+  // useEffect(() => {
+  //   if (selectedColorId) {
+  //     fetch(`https://www.thecolorapi.com/id?${selectedColorId}`)
+  //       .then((response) => response.json())
+  //       .then((data) => {
+  //         setSelectedColorId([data.color.hex]);
+  //       })
+  //       .catch((error) => console.error("Error:", error));
+  //   }
+  // }, [selectedColorId]);
+
+  const updateColorsPalette = () => {
+    const newColors = color.map((color) => {
       return "#" + Math.floor(Math.random() * 16777215).toString(16);
     });
 
-    setColors(newColors);
+    setColor(newColors);
   };
 
   return (
     <div className="App">
       <h1>Colors Palette</h1>
-      <ColorsPalette colors={colors} />
-      <button onClick={updateAllColors}>Update All Colors</button>
+      <ColorsPalette colors={color} />
+      <button onClick={updateColorsPalette}>New Palette</button>
     </div>
   );
 }
